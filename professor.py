9117a9c590b7c15ce9ea57553bc2452fe96dd108
thiagodc <thiagodamicoc@gmail.com>
2025-05-08 21:01:04 -0300

    Primeiras funcoes do professor.py

---
+++ professor.py
@@ -0,0 +1,31 @@
+
+
+
+
+def cria_exercicio(titulo_lista:str,lista_de_exercicio:list)->None:
+    """ Funcao que salva no json a lista de exercicios junto de seu titulo
+
+    Args:
+        titulo_lista (str): Titulo da lista
+        lista_de_exercicio (list): Lista em que cada posicao é uma string com uma questao
+    """
+    pass
+
+def cria_turma(nome_turma:str)->None:
+    """ Funcao que cria no json uma turma sendo o nome dela a chave
+    e a lista da turma o valor
+
+    Args:
+        nome_turma (str): Nome da turma
+    """
+
+def insere_aluno(nome_turma:str,matricula:int)->None:
+    """ Funcao para inserir um aluno em uma turma, funcao deve buscar a turma no json,
+    e inserir a matricula do aluno na lista de alunos.
+
+
+    Args:
+        turma (str): Nome da turma
+        matricula (int): Matricula do aluno
+    """
+    pass
