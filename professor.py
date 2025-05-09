04a87ac3bc47cb4fb9098b17b4a07e8696960dc9
thiagodc <thiagodamicoc@gmail.com>
2025-05-08 21:15:48 -0300

    professor.py prototipo

--- professor.py
+++ professor.py
@@ -11,6 +11,8 @@ def cria_exercicio(titulo_lista:str,lista_de_exercicio:list)->None:
     """
     pass
 
+
+
 def cria_turma(nome_turma:str)->None:
     """ Funcao que cria no json uma turma sendo o nome dela a chave
     e a lista da turma o valor
@@ -18,10 +20,11 @@ def cria_turma(nome_turma:str)->None:
     Args:
         nome_turma (str): Nome da turma
     """
+    pass
 
 def insere_aluno(nome_turma:str,matricula:int)->None:
     """ Funcao para inserir um aluno em uma turma, funcao deve buscar a turma no json,
-    e inserir a matricula do aluno na lista de alunos.
+    e inserir a matricula do aluno na lista de alunos que é seu valor.
 
 
     Args:
