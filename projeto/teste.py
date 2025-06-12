f3cceb3c38d13ea24d5a5ea8ee9cec4898dac50e
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2025-06-11 21:12:27 -0300

    criando classe teste e arquivo sistema

---
+++ projeto/teste.py
@@ -0,0 +1,8 @@
+class teste:
+    def __init__(self, funcao, *args, **kwargs):
+        # define os atributos
+        self.funcao = funcao
+        self.args = args
+        self.kwargs = kwargs
+
+   
